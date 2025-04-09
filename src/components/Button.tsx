0fadabacacfeb1c824abdce7b0a876c24fae9506
aniapru <annaprusinska12@gmail.com>
2025-04-09 23:08:53 +0200

    Update button props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,11 +2,16 @@ import { type ReactNode } from "react";
 
 interface Props {
   children: ReactNode;
+  clicked?: boolean;
+  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-export default function Button({ children }: Props) {
+export default function Button({ children, clicked, onClick }: Props) {
   return (
-    <button className="w-[160px] cursor-pointer rounded-md bg-cyan-600 font-serif text-xl">
+    <button
+      className={`w-[160px] cursor-pointer rounded-md ${clicked ? "bg-slate-400" : "bg-cyan-600"} font-serif text-xl`}
+      onClick={onClick}
+    >
       {children}
     </button>
   );
