31be07435e162aecad712de20f65df3afdd3a779
aniapru <annaprusinska12@gmail.com>
2025-04-16 14:01:31 +0200

    Update click card styles

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -33,19 +33,19 @@ export default function Card({
     setTimeout(() => {
       setChosenCards([]);
       setClick([]);
-    }, 1600);
+    }, 3500);
   }
 
   return (
     <>
       <div className="mh-[100px] mw-[100px] flex aspect-square items-center justify-center bg-transparent perspective-[1000px]">
         <div
-          className={`relative h-full w-full text-center shadow-md transition-transform duration-300 transform-3d ${chosenCards.length === 2 ? null : "hover:rotate-y-180"} ${paired ? "rotate-y-180" : null} ${clicked ? "rotate-y-180" : null}`}
+          onClick={cardClickHandler}
+          className={`relative h-full w-full text-center shadow-md transition-transform duration-400 transform-3d ${chosenCards.length === 2 ? "pointer-events-none" : null} ${paired ? "rotate-y-180" : null} ${clicked ? "pointer-events-none rotate-y-180" : null}`}
         >
           <div className="absolute inset-0 flex items-center justify-center rounded-md bg-linear-to-tl from-cyan-600 to-cyan-400 text-black backface-hidden"></div>
           <div
-            onClick={cardClickHandler}
-            className={`absolute inset-0 flex rotate-y-180 items-center justify-center rounded-md bg-linear-to-tl ${clicked ? "pointer-events-none" : null} from-neutral-400 to-neutral-200 text-white backface-hidden`}
+            className={`absolute inset-0 flex rotate-y-180 items-center justify-center rounded-md bg-linear-to-tl from-neutral-400 to-neutral-200 text-white backface-hidden`}
           >
             {children}
           </div>
